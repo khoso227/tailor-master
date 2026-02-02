841a0d6207f8ac5d5b4f5c2a67316f9ad99ddce5
khoso227 <khososarang816@gmail.com>
2026-02-02 17:16:06 +0500

    Update translations.py

--- translations.py
+++ translations.py
@@ -66,4 +66,21 @@ def get_text(lang):
             "sh_col": "Shirt Collar", "sw_col": "Sherwani Collar", "cuf": "Cuff Sleeve",
             "kur_slv": "Kurta Sleeve", "round": "Round Ghera", "square": "Square Ghera",
             "fit": "Fitting", "double": "Double Stitch", "gum": "Gum Silai"
+
+        }def get_text(lang):
+    if lang == "Urdu":
+        return {
+            "title": "ٹیلر ماسٹر پرو", "login": "لاگ ان", "register": "نیا اکاؤنٹ بنائیں",
+            "forgot": "پاس ورڈ بھول گئے؟", "email": "ای میل", "pass": "پاس ورڈ",
+            "shop": "دکان کا نام", "s_q": "سیکیورٹی سوال (بچپن کا دوست؟)", "s_a": "جواب",
+            "dash": "ڈیش بورڈ", "new_order": "نیا آرڈر", "save": "محفوظ کریں",
+            "remind": "پیمنٹ ریمائنڈر", "delete": "حذف کریں", "status": "حالت"
+        }
+    else:
+        return {
+            "title": "Tailor Master Pro", "login": "Login", "register": "Create Free Account",
+            "forgot": "Forgot Password?", "email": "Email Address", "pass": "Password",
+            "shop": "Shop Name", "s_q": "Security Question (Best Friend?)", "s_a": "Answer",
+            "dash": "Dashboard", "new_order": "New Order", "save": "Save Order",
+            "remind": "Send Reminder", "delete": "Delete Shop", "status": "Status"
         }

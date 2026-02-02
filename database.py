1b8999edf01cd6f81b566298c0d194e8c18ad9ee
khoso227 <khososarang816@gmail.com>
2026-02-02 19:08:35 +0500

    Update database.py

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ import sqlite3
 
 def get_connection():
     conn = sqlite3.connect("tailor_master.db", check_same_thread=False)
-    # Tables creation
+    # 1. Users Table
     conn.execute("""
         CREATE TABLE IF NOT EXISTS users (
             id INTEGER PRIMARY KEY AUTOINCREMENT, 
@@ -11,16 +11,19 @@ def get_connection():
             status TEXT DEFAULT 'Active'
         )
     """)
+    # 2. Orders & Measurements Table
     conn.execute("""
         CREATE TABLE IF NOT EXISTS orders (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             user_id INTEGER, client_name TEXT, client_phone TEXT,
             length TEXT, shoulder TEXT, chest TEXT, waist TEXT, sleeves TEXT, neck TEXT, bottom TEXT,
+            thigh TEXT, fly TEXT, hip TEXT, 
+            style_collar TEXT, style_sleeve TEXT, style_ghera TEXT,
             payment_method TEXT, total_bill REAL, paid_amount REAL,
             order_date TEXT, status TEXT DEFAULT 'Pending'
         )
     """)
-    # Admin creation
+    # Auto Admin
     admin_check = conn.execute("SELECT id FROM users WHERE email='admin@sahilarman.com'").fetchone()
     if not admin_check:
         conn.execute("INSERT INTO users (email, password, shop_name, role, status) VALUES ('admin@sahilarman.com', 'sahilarman2026', 'Super Admin', 'super_admin', 'Active')")
